ef7f1362a63756f1270659867b87dc4de4473f38
Sven Wehrend <swehrend@ant-neuro.com>
2026-06-11 22:11:33 +0200

    fix / update menifest

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Bulk update product sales and cost prices using a wizard.',
     'sequence': 10,
     'description': """
-Product Price Update Wizard
+Pricelist Update Wizard
 ===========================
 This module adds an action in the product list view allowing users 
 to select multiple products and update their prices (sale price or cost) 
@@ -13,7 +13,7 @@ by a percentage or a fixed amount in bulk.
     """,
     'category': 'Sales/Sales',
     'author': 'Sven Wehrend',
-    'website': 'https://github.com/wehrend/product_price_update_wizard',
+    'website': 'https://github.com/wehrend/pricelist_update_wizard',
     'depends': [
         'base',
         'product',
